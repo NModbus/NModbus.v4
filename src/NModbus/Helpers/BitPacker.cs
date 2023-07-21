3fcb06aaf0115b19f0534bcfd848d9d3fcfe8b14
Rich Quackenbush <Richard.Quackenbush@AcuityBrands.com>
2023-07-21 10:22:59 -0400

    Added push-alpha build

--- src/NModbus/Helpers/BitPacker.cs
+++ src/NModbus/Helpers/BitPacker.cs
@@ -1,7 +1,18 @@
 ﻿namespace NModbus.Helpers
 {
+    /// <summary>
+    /// Utilities for packing and unpacking bits to and from byte arrays.
+    /// </summary>
     public static class BitPacker
     {
+        /// <summary>
+        /// Unpacks the specified number of bits from a byte array.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="bitCount"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public static bool[] Unpack(this byte[] buffer, ushort bitCount)
         {
             if (buffer is null) throw new ArgumentNullException(nameof(buffer));
@@ -32,6 +43,11 @@
             return bits;
         }
 
+        /// <summary>
+        /// Packs the specified bits into the least possible number of bytes.
+        /// </summary>
+        /// <param name="bits"></param>
+        /// <returns></returns>
         public static byte[] Pack(this bool[] bits)
         {
             var byteCount = CalculateBytesToPackBits(bits.Length);
@@ -60,6 +76,12 @@
             return bytes;
         }
 
+        /// <summary>
+        /// Determine the number of bytes required to pack the specified number of bits in.
+        /// </summary>
+        /// <param name="numberOfBits">The total number of bits to store in a byte array.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public static int CalculateBytesToPackBits(int numberOfBits)
         {
             if (numberOfBits <= 0)
