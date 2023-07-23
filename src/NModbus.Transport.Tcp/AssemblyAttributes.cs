e0ffde1acd93aa2227c9790f942bfe318afeb023
Rich Quackenbush <rich.quackenbush@gmail.com>
2023-07-23 15:17:56 -0400

    Added a TCP project to make the code coverage work

---
+++ src/NModbus.Transport.Tcp/AssemblyAttributes.cs
@@ -0,0 +1,3 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("NModbus.Transports.Tcp.Tests")]
---
+++ src/NModbus.Transports.Tcp.Tests/MbapHeaderTests.cs
@@ -0,0 +1,16 @@
+﻿using NModbus.Transport.Tcp.TcpMessages;
+using Shouldly;
+
+namespace NModbus.Transports.Tcp.Tests
+{
+    public class MbapHeaderTests
+    {
+        [Theory]
+        [InlineData(42, 3, 1, new byte[] { 0, 42, 0, 0, 0, 3, 1 })]
+        public void MbapHeader_ShouldSerialize(ushort transactionIdentifier, ushort length, byte unitIdentifier, byte[] expectedBytes)
+        {
+            MbapHeaderSerializer.SerializeMbapHeader(transactionIdentifier, length, unitIdentifier)
+               .ShouldBe(expectedBytes);
+        }
+    }
+}
