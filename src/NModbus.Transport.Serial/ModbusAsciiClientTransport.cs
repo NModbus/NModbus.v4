d7d8cd0f467eeaeeebde57198ffa0efba0f6121f
Rich Quackenbush <rich.quackenbush@gmail.com>
2026-02-11 18:23:55 -0500

    Checkpoint

---
+++ src/NModbus.Transport.Serial/ModbusAsciiClientTransport.cs
@@ -0,0 +1,40 @@
+﻿using NModbus.Interfaces;
+using System.IO.Ports;
+
+namespace NModbus.Transport.Serial;
+
+public class ModbusAsciiClientTransport(SerialPort serialPort) : IModbusClientTransport
+{
+    public async Task<IModbusDataUnit?> SendAndReceiveAsync(IModbusDataUnit message, CancellationToken cancellationToken = default)
+    {
+        var serialized = AsciiSerializer.Serialize(message);
+
+        serialPort.Write(serialized);
+
+        //TODO: Deal with timeouts
+        var response = serialPort.ReadLine();
+
+        if (response == null)
+            throw new InvalidOperationException("The data received from the device was null.");
+
+        var responseMessage = AsciiSerializer.Deserialize(response);
+
+        return responseMessage;
+    }
+
+    public Task SendAsync(IModbusDataUnit message, CancellationToken cancellationToken = default)
+    {
+        var serialized = AsciiSerializer.Serialize(message);
+
+        serialPort.Write(serialized);
+
+        return Task.CompletedTask;
+    }
+    
+    public ValueTask DisposeAsync()
+    {
+        return ValueTask.CompletedTask;
+    }
+}
+
+
---
+++ src/NModbus.Transport.Serial/RtuSerializer.cs
@@ -0,0 +1,18 @@
+﻿using NModbus.Interfaces;
+
+namespace NModbus.Transport.Serial;
+
+public static class RtuSerializer
+{
+    public static byte[] Serialize(IModbusDataUnit message)
+    {
+        throw new NotImplementedException(); 
+    }
+
+    public static IModbusDataUnit Deserialize(byte[] source)
+    {
+        throw new NotImplementedException(); 
+    }
+}
+
+
